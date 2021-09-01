0574f1b5564b904a2ff1f4bce12a8b7b73589198
JosePasini <josepasini.17@gmail.com>
2021-09-01 16:15:32 -0300

    Paginada la clase Autor, terminado.

--- src/main/java/com/example/persona_dos/controllers/AutorController.java
+++ src/main/java/com/example/persona_dos/controllers/AutorController.java
@@ -2,16 +2,33 @@ package com.example.persona_dos.controllers;
 
 import com.example.persona_dos.entities.Autor;
 import com.example.persona_dos.services.AutorServiceImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.print.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path ="api/v1/autores")
 public class AutorController extends BaseControllerImpl<Autor, AutorServiceImpl>{
+
+    @GetMapping("/search")
+    public ResponseEntity<?> search(@RequestParam String filtro){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.search(filtro));
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\": \"" + e.getMessage() + "\"}"));
+        }
+    }
+
+    @GetMapping("/searchPaged")
+    public ResponseEntity<?> search(@RequestParam String filtro, Pageable pageable){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.search(filtro, pageable));
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\": \"" + e.getMessage() + "\"}"));
+        }
+    }
+
 
 }
--- src/main/java/com/example/persona_dos/services/AutorServiceImpl.java
+++ src/main/java/com/example/persona_dos/services/AutorServiceImpl.java
@@ -4,7 +4,11 @@ import com.example.persona_dos.entities.Autor;
 import com.example.persona_dos.repositories.AutorRepository;
 import com.example.persona_dos.repositories.BaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AutorServiceImpl extends BaseServiceImpl<Autor, Long> implements AutorService{
@@ -14,4 +18,25 @@ public class AutorServiceImpl extends BaseServiceImpl<Autor, Long> implements Au
     public AutorServiceImpl(BaseRepository<Autor, Long> baseRepository) {
         super(baseRepository);
     }
+
+    @Override
+    public List<Autor> search(String filtro) throws Exception{
+        try {
+            List<Autor> autores = autorRepository.search(filtro);
+            return autores;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    public Page<Autor> search(String filtro, Pageable pageable) throws Exception{
+        try {
+            Page<Autor> autores = autorRepository.search(filtro, pageable);
+            return autores;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
 }
