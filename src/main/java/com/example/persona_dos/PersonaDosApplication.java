22764df50966129b9334106b1a63e99357ec9e44
JosePasini <josepasini.17@gmail.com>
2021-09-19 18:46:09 -0300

    d

--- src/main/java/com/example/persona_dos/PersonaDosApplication.java
+++ src/main/java/com/example/persona_dos/PersonaDosApplication.java
@@ -2,6 +2,8 @@ package com.example.persona_dos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PersonaDosApplication {
@@ -9,5 +11,4 @@ public class PersonaDosApplication {
     public static void main(String[] args) {
         SpringApplication.run(PersonaDosApplication.class, args);
     }
-
 }
